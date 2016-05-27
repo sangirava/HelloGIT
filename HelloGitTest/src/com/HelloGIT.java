af5aacc2c256f02024d1c0010c8f39bd0b551870
sdiwaka <sangeetha.diwakar@t-mobile.com>
2016-05-27 11:51:15 -0700

    Initial Checkin of HelloGIT

---
+++ HelloGitTest/src/com/HelloGIT.java
@@ -0,0 +1,31 @@
+package com;
+
+public class HelloGIT {
+
+	static {
+        message = "Hello NNnNew Worlddd!";
+    }
+
+    private final static String message;    
+
+    public HelloGIT() {}
+
+    public static void main(String[] args) {
+        System.out.println(new HelloGIT().getMessage());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("HelloWorld [");
+        if (getMessage() != null)
+            builder.append("message=").append(getMessage());
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
